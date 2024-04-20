032c1ebe71fb1bcddb70d942db2f32d144b587f5
Ruby <contact.ruby0102@gmail.com>
2024-04-20 13:08:15 +0530

    rollback mongo uri

--- backend/db.js
+++ backend/db.js
@@ -1,10 +1,9 @@
 const mongoose = require('mongoose')
-
 // const mongoDbClient = require("mongodb").MongoClient
-//const mongoURI = 'mongodb+srv://ruby1103:plantcell@cluster0.1qqtcld.mongodb.net/foodie' // Customer change url to your db you created in atlas
+const mongoURI = 'mongodb+srv://ruby1103:plantcell@cluster0.1qqtcld.mongodb.net/foodie' // Customer change url to your db you created in atlas
 // mongodb://<username>:<password>@merncluster-shard-00-00.d1d4z.mongodb.net:27017,merncluster-shard-00-01.d1d4z.mongodb.net:27017,merncluster-shard-00-02.d1d4z.mongodb.net:27017/?ssl=true&replicaSet=atlas-eusy5p-shard-0&authSource=admin&retryWrites=true&w=majority
 module.exports = function (callback) {
-    mongoose.connect(`${process.env.MONGODB_URI}`, { useNewUrlParser: true }, async (err, result) => {
+    mongoose.connect(mongoURI, { useNewUrlParser: true }, async (err, result) => {
         // mongoDbClient.connect(mongoURI, { useNewUrlParser: true }, async(err, result) => {
         if (err) console.log("---" + err)
         else {
