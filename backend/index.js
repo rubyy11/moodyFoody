08668ac56c0ca124412c5b9bb63362fb7145050c
Ruby <contact.ruby0102@gmail.com>
2024-04-20 13:33:43 +0530

    change in index.js and db.js

--- backend/index.js
+++ backend/index.js
@@ -1,38 +1,38 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const port = 5000;
+const striperoutes = require('./Routes/stripe-route');
 
-global.foodData = require('./db')(function call(err, data, CatData) {
-  // console.log(data)
-  if(err) console.log(err);
-  global.foodData = data;
-  global.foodCategory = CatData;
-})
+// Import database initialization function
+const initializeDatabase = require('./db');
 
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const port = 5000
-const striperoutes = require('./Routes/stripe-route')
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.use(cors())
+// Middleware to update global.foodData for each request
+app.use((req, res, next) => {
+    initializeDatabase((err, data, CatData) => {
+        if (err) {
+            console.log(err);
+            res.status(500).send('Internal Server Error');
+            return;
+        }
+        global.foodData = data;
+        global.foodCategory = CatData;
+        next();
+    });
+});
 
-// app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-//   res.header(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content-Type, Accept"
-//   );
-//   next();
-// });
-app.use(express.json())
-app.use(express.urlencoded({extended:true}))
-app.use('/api/stripe',striperoutes)
-
+// Routes
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+    res.send('Hello World!');
+});
 
 app.use('/api/auth', require('./Routes/Auth'));
+app.use('/api/stripe', striperoutes);
 
 app.listen(port, () => {
-  console.log(`Example app listening on http://localhost:${port}`)
-})
-
+    console.log(`Example app listening on http://localhost:${port}`);
+});
